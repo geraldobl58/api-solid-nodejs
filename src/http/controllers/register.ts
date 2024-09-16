d007925ef3b8ff21ee532374f9007600e6bbe00e
Geraldo Luiz <geraldobl58@gmail.com>
2024-09-16 14:25:05 -0300

    feat: Creating with use case errors

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -3,6 +3,7 @@ import { FastifyReply, FastifyRequest } from "fastify";
 import { z } from "zod";
 
 import { RegistertUseCase } from "@/use-cases/register";
+import { UserAlreadyExistsError } from "@/use-cases/errors/user-already-exists-error";
 
 import { PrismaUsersRepository } from "@/repositories/prisma/prisma-users-repository";
 
@@ -25,7 +26,11 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
       password,
     });
   } catch (error) {
-    return reply.status(409).send();
+    if (error instanceof UserAlreadyExistsError) {
+      return reply.status(409).send({ message: error.message });
+    }
+
+    return reply.status(500).send();
   }
 
   return reply.status(201).send();
